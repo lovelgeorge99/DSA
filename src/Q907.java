0dea1a2645a77c9e1ee0a6617f6b42c41f6b81e6
Lovel George <lovelgeorge22@gmail.com>
2024-01-20 14:35:33 -0500

    Solved  Q907 sum of subarray minimums

---
+++ src/Q907.java
@@ -0,0 +1,64 @@
+import java.util.Stack;
+
+public class Q907 {
+    final static int M = 1000000007;
+    public static int[] getNSL(int[] arr, int n) {
+        int[] result = new int[n];
+        Stack<Integer> st = new Stack<>();
+
+        for (int i = 0; i < n; i++) {
+            while (!st.isEmpty() && arr[st.peek()] > arr[i]) {
+                st.pop();
+            }
+
+            result[i] = st.isEmpty() ? -1 : st.peek();
+            st.push(i);
+        }
+
+        return result;
+    }
+
+    public static int[] getNSR(int[] arr, int n) {
+        int[] result = new int[n];
+        Stack<Integer> st = new Stack<>();
+
+        for (int i = n - 1; i >= 0; i--) {
+            while (!st.isEmpty() && arr[st.peek()] >= arr[i]) {
+                st.pop();
+            }
+
+            result[i] = st.isEmpty() ? n : st.peek();
+            st.push(i);
+        }
+
+        return result;
+    }
+
+    public static int sumSubarrayMins(int[] arr) {
+        int n = arr.length;
+
+        int[] NSL = getNSL(arr, n);
+        int[] NSR = getNSR(arr, n);
+
+        long sum = 0;
+        int M = 1000000007;
+
+        for (int i = 0; i < n; i++) {
+            long d1 = i - NSL[i];
+            long d2 = NSR[i] - i;
+
+            long totalWaysForIMin = d1 * d2;
+            long sumIInTotalWays = arr[i] * totalWaysForIMin;
+
+            sum = (sum + sumIInTotalWays) % M;
+        }
+
+        return (int) sum;
+    }
+
+
+    public static void main(String[] args) {
+    int[] arr={3,1,2,4};
+        System.out.println(sumSubarrayMins(arr));
+    }
+}
