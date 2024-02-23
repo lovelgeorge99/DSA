bf6fbf78ec6a9ee6849005e038d5f8ff69744aed
Lovel George <lovelgeorge22@gmail.com>
2024-02-23 14:42:46 -0500

    some cnhanges in code

--- src/Q242.java
+++ src/Q242.java
@@ -17,6 +17,6 @@ public class Q242 {
     }
 
     public static void main(String[] args) {
-        System.out.println(isAnagram("abbbc","bbbca"));
+        System.out.println(isAnagram("bbc","bbbca"));
     }
 }
