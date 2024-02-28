0a8791b73c2fcdd6ebe8778f409e1b0e841351c9
Lovel George <lovelgeorge22@gmail.com>
2024-02-28 16:34:39 -0500

    Q513

---
+++ src/Trees/Q513.java
@@ -0,0 +1,25 @@
+package Trees;
+
+public class Q513 {
+    int mdept;
+    int ans;
+    public void solve(BT node,int cdept){
+        if(node==null){
+            return;
+        }
+        if(cdept>mdept){
+            mdept=cdept;
+            ans=node.val;
+        }
+        solve(node.left,cdept+1);
+        solve(node.right,cdept+1);
+    }
+    public int findBottomLeftValue(BT root) {
+        mdept=Integer.MIN_VALUE;
+
+        solve(root,0);
+        return ans;
+
+
+    }
+}
